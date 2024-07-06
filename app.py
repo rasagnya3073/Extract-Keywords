0ea51fb22ab6ddec5974b1c12ed76cd1307c54a9
rasagnya3073 <rasagnyanagesh3073@gmail.com>
2024-07-06 19:40:46 +0530

    commit

---
+++ app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:db23554ab87eae5c94d8e02a85449089c90829ef1c63495ae1c7a9ae32690c66
+size 1962
---
+++ model.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:eb72a82cd797ccedc1f0a3b7e97e52ac172f9a37d1c9cb463fd1ffd180f3c43f
+size 1974
